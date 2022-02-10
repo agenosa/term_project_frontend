230feee5e9325750bf76034c9dd1cb4695a16d96
Mandy Dhanju <93342747+mandeepdhanju@users.noreply.github.com>
2022-02-10 12:29:12 -0800

    Create CSS for  App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
 import UserInfo from './components/UserInfo'
-import { Button } from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button>Test Button</Button>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">    
+   
+      <div class = "navbarNav">
+                <ul class="navbar-nav">
+                <h1 ><a href="#0">Tantalus</a></h1>
+               
+                    <a class="nav-link" href="#0">Home </a>               
+                    <a class="nav-link" href="#0">Browse</a>    
+                    <a class="nav-link" href="#0">Categories</a>
+                
+
+                  <select id="products">
+                      <option value="">--Products--</option>
+                      <option value="Hiking">Hiking</option>
+                      <option value="Skiing">Skiing</option>
+                      <option value="Biking">Biking</option>
+                      <option value="Climbing">Climbing</option>
+                  </select>
+  
+
+                  {/* <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#0" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Products
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#0">Hiking</a>
+                        <a class="dropdown-item" href="#0">Skiing</a>
+                        <a class="dropdown-item" href="#0">Biking</a>
+                        <a class="dropdown-item" href="#0">Climbing</a>
+                    </div>
+                  </li> */}
+                </ul>
+
+                <div className="inputsearch">
+             <input type="text" id="search" name="search" className="search-name" placeholder="Search"/><p>Search</p>
+            </div>
+                
+          </div>
+
       </header>
       <UserInfo />
+  
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,32 @@
+// import { NavLink } from 'react-router-dom';
+// import React from "react";
+
+// function Nav() {
+    
+//     return (
+//         <nav>
+//           <ul className='navbar'>
+//               <li><NavLink to="/">Home</NavLink></li>
+//               <li><NavLink to="/">Browse</NavLink></li>
+//               <li><NavLink to="/">Categories</NavLink></li>
+   
+
+//               <li class="nav-item dropdown">
+//                     <a class="nav-link dropdown-toggle" href="#0" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+//                         Products
+//                     </a>
+//                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+//                         <a class="dropdown-item" href="#0">Hiking</a>
+//                         <a class="dropdown-item" href="#0">Skiing</a>
+//                         <a class="dropdown-item" href="#0">Biking</a>
+//                         <a class="dropdown-item" href="#0">Climbing</a>
+//                     </div>
+//                   </li>
+//           </ul>
+
+//         </nav>
+
+//       )
+//   }
+  
+//   export default Nav;
